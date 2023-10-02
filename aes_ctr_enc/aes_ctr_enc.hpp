f7ce624bc11b19455d55428d621494404a5d0888
Ahmed Sami <ahmedsami03@protonmail.com>
2023-10-02 19:16:27 +0200

    Add bigger buffer sizes

--- aes_ctr_enc/aes_ctr_enc.hpp
+++ aes_ctr_enc/aes_ctr_enc.hpp
@@ -19,11 +19,11 @@ private:
 public:
     AES_CTR(const __m128i& key, __m128i& iv, __m128i& firstBlock);
 
-    void encrypt4Blocks(__m128i* plaintextChunk);
+    void encrypt8Blocks(__m128i plaintextChunk[8]);
 
-    void encrypt3Blocks(__m128i* plaintextChunk);
+    void encrypt4Blocks(__m128i plaintextChunk[4]);
 
-    void encrypt2Blocks(__m128i* plaintextChunk);
+    void encrypt2Blocks(__m128i plaintextChunk[2]);
 
     void encrypt1Block(__m128i* plaintextChunk);
 };
